ad05d60d8f3c2eb7e965939aaec6fc3dba9959cf
Samarth Chitley <maharanapratapsingh1576@gmail.com>
2023-12-26 21:10:22 +0530

    User Login view, urls, UI

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 
 def home(request):
     return render(request,'webapp/index.html')
@@ -13,3 +15,20 @@ def register(request):
             # return redirect('')
     context = {'form':form}
     return render(request,'webapp/user/register.html', context=context)
+
+def login(request):
+    login_form = LoginForm()
+    if request.method == "POST":
+        login_form=LoginForm(request, data = request.POST)
+        if login_form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None :
+                auth.login(request, user)
+                # return redirect('')
+    context = {'login_form': login_form}
+    return render (request, 'webapp/user/login.html', context=context)
+    
+
+
